84ff3639b102d4a6737a42a15a0a2d03399e64cc
Edson <zhuoxi2000@gmail.com>
2023-09-03 10:45:06 -0700

    ethernet send and recive process

--- Project1/xnet_tiny.c
+++ Project1/xnet_tiny.c
@@ -1,9 +1,12 @@
 #include "xnet_tiny.h"
-
+#include<string.h>
 #define min(a, b)               ((a) > (b) ? (b) : (a))
 
+static uint8_t netif_mac[XNET_MAC_ADDR_SIZE];
 static xnet_packet_t tx_packet, rx_packet;                      // 接收与发送缓冲区
 
+
+#define swap_order16(v)   ((((v) & 0xFF) << 8) | (((v) >> 8) & 0xFF))
 /**
  * 分配一个网络数据包用于发送数据
  * @param data_size 数据空间大小
@@ -56,6 +59,77 @@ static void remove_header(xnet_packet_t* packet, uint16_t header_size) {
 static void truncate_packet(xnet_packet_t* packet, uint16_t size) {
     packet->size = min(packet->size, size);
 }
+
+/*以太网初始化*/
+static xnet_err_t ethernet_init(void) {
+    xnet_err_t err = xnet_driver_open(netif_mac);
+    if (err < 0) return err;
+
+    return XNET_ERR_OK;
+}
+
+/**
+ * 发送一个以太网数据帧
+ * @param protocol 上层数据协议，IP或ARP
+ * @param mac_addr 目标网卡的mac地址
+ * @param packet 待发送的数据包
+ * @return 发送结果
+ */
+static xnet_err_t ethernet_out_to(xnet_protocol_t protocol, const uint8_t* mac_addr, xnet_packet_t* packet) {
+    xether_hdr_t* ether_hdr;
+
+    // 添加头部
+    add_header(packet, sizeof(xether_hdr_t));
+    /*
+    以太网的包头添加到原本的数据前面，构成新的数据包
+    eher_hdr 表示为新数据包的首地址，再去填充
+    */
+    ether_hdr = (xether_hdr_t*)packet->data; 
+    memcpy(ether_hdr->dest, mac_addr, XNET_MAC_ADDR_SIZE);
+    memcpy(ether_hdr->src, netif_mac, XNET_MAC_ADDR_SIZE);
+    ether_hdr->protocol = swap_order16(protocol); //大小端的问题
+
+    // 数据发送
+    return xnet_driver_send(packet);
+}
+
+/**
+ * 以太网数据帧输入输出
+ * @param packet 待处理的包
+ */
+static void ethernet_in(xnet_packet_t* packet) {
+    // 至少要比头部数据大
+    //还有其他情况
+    if (packet->size <= sizeof(xether_hdr_t)) {
+        return;
+    }
+
+    // 往上分解到各个协议处理
+
+    //强制转换data指针，让data后面的数据，按照xether_hdr_t的结构体去解析，就可以解析到，前面一部分的表头
+    xether_hdr_t* hdr = (xether_hdr_t*)packet->data;
+    switch (swap_order16(hdr->protocol)) {
+    case XNET_PROTOCOL_ARP:
+        break;
+    case XNET_PROTOCOL_IP: {
+        break;
+    }
+    }
+}
+
+/**
+ * 查询网络接口，看看是否有数据包，有则进行处理
+ */
+static void ethernet_poll(void) {
+    xnet_packet_t* packet;
+
+    if (xnet_driver_read(&packet) == XNET_ERR_OK) {
+        // 正常情况下，在此打个断点，全速运行
+        // 然后在对方端ping 192.168.254.2，会停在这里
+        ethernet_in(packet);
+    }
+}
+
 
 /**
  * 协议栈的初始化
--- Project1/xnet_tiny.h
+++ Project1/xnet_tiny.h
@@ -5,6 +5,18 @@
 
 #define XNET_CFG_PACKET_MAX_SIZE        1516        // 收发数据包的最大大小
 
+#define XNET_MAC_ADDR_SIZE              6           // MAC地址长度
+
+/**
+ * 以太网数据帧格式：RFC894
+ */
+typedef struct _xether_hdr_t {
+    uint8_t dest[XNET_MAC_ADDR_SIZE];           // 目标mac地址
+    uint8_t src[XNET_MAC_ADDR_SIZE];            // 源mac地址
+    uint16_t protocol;                          // 协议/长度
+}xether_hdr_t;
+
+
 typedef enum _xnet_err_t {
     XNET_ERR_OK = 0,
     XNET_ERR_IO = -1,
@@ -25,6 +37,12 @@ xnet_packet_t* xnet_alloc_for_read(uint16_t data_size);
 xnet_err_t xnet_driver_open(uint8_t* mac_addr);
 xnet_err_t xnet_driver_send(xnet_packet_t* packet);
 xnet_err_t xnet_driver_read(xnet_packet_t** packet);
+
+typedef enum _xnet_protocol_t {
+    XNET_PROTOCOL_ARP = 0x0806,     // ARP协议
+    XNET_PROTOCOL_IP = 0x0800,      // IP协议
+}xnet_protocol_t;
+
 
 void xnet_init(void);
 void xnet_poll(void);
